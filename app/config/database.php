aa844261fdeb4d181ee0c1f8bf92e044072cf970
Jonas Barbosa <jbsilva023gmail.com>
2019-10-21 00:42:13 -0300

    [FIX] Databese conection

--- app/config/database.php
+++ app/config/database.php
@@ -5,10 +5,7 @@ use Dotenv\Dotenv;
 $dotenv = new Dotenv(__DIR__);
 $dotenv->load();
 
-if (!isset(self::$conexao)) {
-    $pdo = new \PDO($this->dns=getenv('DB_CONNECTION').':host='. getenv('DB_HOST') . ';dbname=' . getenv('DB_DATABASE'),
-        getenv('DB_USERNAME'), getenv('DB_PASSWORD'), [PDO::ATTR_PERSISTENT => TRUE]);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} else {
-    return self::$conexao;
-}
+$pdo = new \PDO(getenv('DB_CONNECTION').':host='. getenv('DB_HOST')
+    . ';dbname=' . getenv('DB_DATABASE'), getenv('DB_USERNAME'), getenv('DB_PASSWORD'),
+    [PDO::ATTR_PERSISTENT => TRUE]);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
